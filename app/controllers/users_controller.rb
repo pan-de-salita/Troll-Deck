a01f2425c0002960f05c2f53bac806ce3c84c4dc
pan-de-salita <156948045+pan-de-salita@users.noreply.github.com>
2024-05-22 18:50:08 +0800

    fix: delete all microposts of a user when their profile is deleted

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -55,6 +55,7 @@ class UsersController < ApplicationController
   # DELETE /users/1 or /users/1.json
   def destroy
     @user.destroy!
+    Micropost.destroy_by(user_id: @user.id)
 
     respond_to do |format|
       format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
