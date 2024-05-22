cd275f5438f122d01c0b709d9aa4c5f956fbac22
pan-de-salita <156948045+pan-de-salita@users.noreply.github.com>
2024-05-22 15:54:01 +0800

    feat: add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,29 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# Create users
+User.create(name: 'Ruby', email: 'ruby@ruby.com')
+User.create(name: 'TypeScript', email: 'typescript@typescript.com')
+User.create(name: 'C', email: 'c@c.com')
+User.create(name: 'Assembly', email: 'assembly@lowlevel.com')
+User.create(name: 'Kristm', email: 'krist.menina@mail.com')
+User.create(name: 'Titser Ni Aristotle', email: 'vince@pleyto.com')
+User.create(name: 'Felix Guattari', email: 'akopalasi@felixguattari.com')
+User.create(name: 'Gilles Deleuze', email: 'deleuze@parin.con')
+User.create(name: 'Stanlicious', email: 'stan@hugo.com')
+
+# Create microposts
+Micropost.create(content: 'Test.', user_id: 1)
+Micropost.create(content: 'TS > Ruby.', user_id: 2)
+Micropost.create(content: 'Ruby > TS.', user_id: 1)
+Micropost.create(content: 'C > any language.', user_id: 3)
+Micropost.create(content: 'Lol.', user_id: 4)
+Micropost.create(content: 'Further reading lang mga to.', user_id: 5)
+Micropost.create(
+  content: 'As there are misanthropists or haters of men, so also are there misologists, or haters of ideas.', user_id: 6
+)
+Micropost.create(
+  content: 'We do not lack communication, on the contrary we have too much of it. We lack creation. We lack resistance to the present.', user_id: 6
+)
+Micropost.create(content: 'h o t d o g', user_id: 9)
